27533e291d9a1b2de26a20007568a9e5a7a0044b
hridyansh <hridyansh68@gmail.com>
2019-05-27 06:25:05 +0530

    Updates

---
+++ precache-manifest.49a9d4fb8099dc2bcdb8b81ff290dc55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80e7203c0b4c021dac3123404cf4679",
+    "url": "/Portfoliowebsite/index.html"
+  },
+  {
+    "revision": "044d75cd66007f77dbfa",
+    "url": "/Portfoliowebsite/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "6379b3b6f601f24e53c8",
+    "url": "/Portfoliowebsite/static/css/main.7dfad134.chunk.css"
+  },
+  {
+    "revision": "044d75cd66007f77dbfa",
+    "url": "/Portfoliowebsite/static/js/2.05a16eb1.chunk.js"
+  },
+  {
+    "revision": "6379b3b6f601f24e53c8",
+    "url": "/Portfoliowebsite/static/js/main.a821d371.chunk.js"
+  },
+  {
+    "revision": "c5f8333058aa7a93f17d",
+    "url": "/Portfoliowebsite/static/js/runtime~main.e45d224b.js"
+  },
+  {
+    "revision": "1b514d961279c3abd819e1f7397c01aa",
+    "url": "/Portfoliowebsite/static/media/myAvatar.1b514d96.png"
+  }
+]);
